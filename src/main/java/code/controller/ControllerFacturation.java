3ec093d7f23a19c10d76a97ba4162a3e1f6a176e
fallouiut <fallou-du-13@hotmail.fr>
2019-05-17 15:24:33 +0200

    pull

--- src/main/java/code/controller/ControllerFacturation.java
+++ src/main/java/code/controller/ControllerFacturation.java
@@ -2,13 +2,33 @@ package code.controller;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-import javax.swing.JButton;
-import javax.swing.JTable;
+import javax.swing.*;
 
+import code.Admin;
+import code.Client;
+import code.Reservation;
+import code.TypeService;
+import code.model.DAOInterfaces.DAOClient;
+import code.model.DAOInterfaces.DAOHotel;
+import code.model.DAOInterfaces.DAOReservation;
+import code.model.DAOInterfaces.DAOTypeService;
+import code.model.DAOJDBC.DAOClientJDBC;
+import code.model.DAOJDBC.DAOHotelJDBC;
+import code.model.DAOJDBC.DAOReservationJDBC;
+import code.model.DAOJDBC.DAOTypeServiceJDBC;
 import code.view.Panels.FacturationPanel;
 
 public class ControllerFacturation extends AbstractController {
+
+	private DAOClient daoClient = new DAOClientJDBC();
+	private DAOReservation daoReservation = new DAOReservationJDBC();
+	private DAOHotel daoHotel = new DAOHotelJDBC();
+	private DAOTypeService daoTypeService = new DAOTypeServiceJDBC();
+	private Admin admin;
 
 	private FacturationPanel m_panel;
 	public ControllerFacturation(FacturationPanel facturationPanel) {
@@ -31,7 +51,7 @@ public class ControllerFacturation extends AbstractController {
 			   		{ "MarieTest", "Test"},
 			};
 			
-		String [] enTete = {"NomTest", "Type"};
+		String [] enTete = {"Type", "Prix"};
 		// Ici recuperer les clients presents
 		JTable table = m_panel.setTableauClients(donnees, enTete);
 		table.addMouseListener(new MouseAdapter() {
@@ -77,6 +97,45 @@ public class ControllerFacturation extends AbstractController {
                         }
                     }	
                 });
+			}
+		});
+	}
+
+	private void afficherClientsPresents() {
+
+		Map<Client, Reservation> clientsPresents = daoClient.findByHotel(admin.getHotelsGeres().get(6));
+		Object[][] donnees = new Object[clientsPresents.size()][12];
+		System.out.println(clientsPresents);
+		int i = 0;
+		for (Map.Entry<Client, Reservation> entry : clientsPresents.entrySet()) {
+			donnees[i][0] =
+					entry.getKey().getNum();
+			donnees[i][1] = entry.getKey().getPrenom();
+			donnees[i][2] = entry.getKey().getNom();
+			donnees[i][3] = entry.getKey().getNomEnteprise();
+			donnees[i][4] = entry.getValue().getNumReservation();
+			donnees[i][5] = entry.getValue().getDateArrivee().toString();
+			donnees[i][6] = entry.getValue().getDateDepart().toString();
+			donnees[i][7] = entry.getValue().getNbPersonnes();
+			++i;
+		}
+
+		String [] enTete = {"Num client", "Prenom", "Nom", "Entreprise", "Num reservation", "Date Arrivee", "Date Depart", "Nb personnes"};
+		JTable table = m_panel.setTableauClients(donnees, enTete);
+		table.addMouseListener(new MouseAdapter() {
+
+			Integer numClient = null;
+			Integer numReservation = null;
+			public void mousePressed(MouseEvent e) {
+				if (e.getClickCount() == 2)
+				{
+					JTable target = (JTable)e.getSource();
+					int row = target.getSelectedRow();
+					int column = target.getSelectedColumn();
+					numClient = (Integer)target.getModel().getValueAt(row, 0);
+					numReservation = (Integer)target.getModel().getValueAt(row, 4);
+				}
+				return;
 			}
 		});
 	}
