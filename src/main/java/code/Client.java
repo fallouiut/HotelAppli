ef37cc413f72b6e5306f86db8b8ea1111ec39fd5
fallouiut <fallou-du-13@hotmail.fr>
2019-05-17 10:02:59 +0200

    renaissance IV

---
+++ src/main/java/code/Client.java
@@ -0,0 +1,105 @@
+package code;
+
+public class Client {
+    private  int num;
+    private  String nom;
+    private  String prenom;
+    private  String nomEnteprise;
+    private  String telephone;
+    private  String mail;
+    private  String pseudo;
+    private  String motDePasse;
+
+    public Client(int num, String nom, String prenom, String nomEnteprise, String telephone, String mail, String pseudo, String motDePasse) {
+        this.num = num;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.nomEnteprise = nomEnteprise;
+        this.telephone = telephone;
+        this.mail = mail;
+        this.pseudo = pseudo;
+        this.motDePasse = motDePasse;
+    }
+
+    public Client() {
+
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getNomEnteprise() {
+        return nomEnteprise;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setNomEnteprise(String nomEnteprise) {
+        this.nomEnteprise = nomEnteprise;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "num=" + num +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", nomEnteprise='" + nomEnteprise + '\'' +
+                ", telephone=" + telephone +
+                ", mail='" + mail + '\'' +
+                ", pseudo='" + pseudo + '\'' +
+                ", motDePasse='" + motDePasse + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/code/Reservation.java
@@ -0,0 +1,140 @@
+package code;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reservation {
+    private int numReservation;
+    private LocalDate dateArrivee;
+    private LocalDate dateDepart;
+    private int nbPersonnes;
+    private String etat;
+    private float prixTotal;
+    private float reduction;
+
+    private Client client;
+    private List<Chambre> chambres = new ArrayList<Chambre>();
+    private List<TypeService> services = new ArrayList<TypeService>();
+    private Hotel hotel;
+
+    public Reservation(int numReservation, LocalDate dateArrivee, LocalDate dateDepart, int nbPersonnes, String etat, float prixTotal, float reduction, Client client, List<Chambre> chambres, List<TypeService> services) {
+        this.numReservation = numReservation;
+        this.dateArrivee = dateArrivee;
+        this.dateDepart = dateDepart;
+        this.nbPersonnes = nbPersonnes;
+        this.etat = etat;
+        this.prixTotal = prixTotal;
+        this.reduction = reduction;
+        this.client = client;
+        this.chambres = chambres == null ? this.chambres: chambres;
+        this.services = services == null ? this.services: services;
+    }
+
+    public Reservation () {
+
+    }
+
+    public List<TypeService> getServices() {
+        return services;
+    }
+
+    public float getPrixTotal() {
+        return prixTotal;
+    }
+
+    public void setChambres(List<Chambre> chambres) {
+        this.chambres = chambres;
+    }
+
+    public List<Chambre> getChambres() {
+        return chambres;
+    }
+
+    public void setNumReservation(int numReservation) {
+        this.numReservation = numReservation;
+    }
+
+    public int getNumReservation() {
+        return numReservation;
+    }
+
+    public LocalDate getDateArrivee() {
+        return dateArrivee;
+    }
+
+    public LocalDate getDateDepart() {
+        return dateDepart;
+    }
+
+    public int getNbPersonnes() {
+        return nbPersonnes;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public float getReduction() {
+        return reduction;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public void setDateArrivee(LocalDate dateArrivee) {
+        this.dateArrivee = dateArrivee;
+    }
+
+    public void setDateDepart(LocalDate dateDepart) {
+        this.dateDepart = dateDepart;
+    }
+
+    public void setNbPersonnes(int nbPersonnes) {
+        this.nbPersonnes = nbPersonnes;
+    }
+
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
+
+    public void setPrixTotal(float prixTotal) {
+        this.prixTotal = prixTotal;
+    }
+
+    public void setReduction(float reduction) {
+        this.reduction = reduction;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public void setServices(List<TypeService> services) {
+        this.services = services;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "numReservation=" + numReservation +
+                ", dateArrivee=" + dateArrivee +
+                ", dateDepart=" + dateDepart +
+                ", nbPersonnes=" + nbPersonnes +
+                ", etat='" + etat  +
+                ", prixTotal=" + prixTotal +
+                ", reduction=" + reduction + "\n" +
+                //", client=" + client.toString() + "\n" +
+                //", chambres=" + chambres.toString() +
+                '}';
+    }
+}
